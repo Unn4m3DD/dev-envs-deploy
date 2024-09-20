38a32a0760b225eae034825b4b14b72fa4037edc
Andre Patacas <andre.patacas@morphotech.co.uk>
2024-09-20 19:57:20 +0100

    chore: setup first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,15 @@
 import "./globals.css";
 import { VercelToolbar } from "@vercel/toolbar/next";
-import { FlagValues } from "@vercel/flags/react";
-import { Donut } from "./Donut";
-import { getFlags } from "./getFlags";
 
-export default async function RootLayout() {
+export default async function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   const shouldInjectToolbar = process.env.NODE_ENV === "development";
-  const flags = await getFlags();
   return (
     <html lang="en">
       <body>
-        <div className="h-screen w-full flex flex-col items-center justify-center">
-          <div className="text-xl font-semibold">
-            A dev environment for another branch appeared!
-          </div>
-          {flags.showDonut && <Donut {...flags} />}
-        </div>
+        {children}
         {shouldInjectToolbar && <VercelToolbar />}
-        <FlagValues values={flags} />
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 import React from "react";
 
-import { FlagValues } from "@vercel/flags/react";
-import { Donut } from "./Donut";
-import { getFlags } from "./getFlags";
-
 export default async function page() {
-  const flags = await getFlags();
   return (
     <div className="h-screen w-full flex flex-col items-center justify-center">
       <div className="text-xl font-semibold">
-        A dev environment for another branch appeared!
+        This is my mainline branch deploy
       </div>
-      {flags.showDonut && <Donut {...flags} />}
-      <FlagValues values={flags} />;
     </div>
   );
 }
