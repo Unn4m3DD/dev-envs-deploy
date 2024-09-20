db5d38feedb417f2b5a6e22eb5c6813e3e83e292
Andre Patacas <andre.patacas@morphotech.co.uk>
2024-09-20 19:53:08 +0100

    fix: routing

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import { FlagValues } from "@vercel/flags/react";
+import { Donut } from "./Donut";
+import { getFlags } from "./getFlags";
+
+export default async function page() {
+  const flags = await getFlags();
+  return (
+    <div className="h-screen w-full flex flex-col items-center justify-center">
+      <div className="text-xl font-semibold">
+        A dev environment for another branch appeared!
+      </div>
+      {flags.showDonut && <Donut {...flags} />}
+      <FlagValues values={flags} />;
+    </div>
+  );
+}
