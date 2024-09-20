c051079e2420d5c01a63620c7fe3c3119fef952f
Andre Patacas <andre.patacas@morphotech.co.uk>
2024-09-20 18:02:59 +0100

    feat: add another commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,9 @@ export default function Home() {
   const donut = useDonut();
   return (
     <div className="h-screen w-full flex flex-col items-center justify-center">
-      <div className="text-xl font-semibold">Hello world!</div>
-      <div className="">This is the mainline branch deploy</div>
+      <div className="text-xl font-semibold">
+        A dev environment for another branch appeared!
+      </div>
       <div className="font-mono text-center whitespace-break-spaces select-none">
         {donut}
       </div>
