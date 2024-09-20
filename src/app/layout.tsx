da4858ab8d860d34cb7ef484be04d2425b3779a7
Andre Patacas <andre.patacas@morphotech.co.uk>
2024-09-20 20:05:28 +0100

    fix: routing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,15 @@
 import "./globals.css";
 import { VercelToolbar } from "@vercel/toolbar/next";
-import { FlagValues } from "@vercel/flags/react";
-import { Donut } from "./Donut";
-import { getFlags } from "./getFlags";
+
 
 export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   const shouldInjectToolbar = process.env.NODE_ENV === "development";
-  const flags = await getFlags();
   return (
     <html lang="en">
       <body>
         {children}
-        <div className="h-screen w-full flex flex-col items-center justify-center">
-          <div className="text-xl font-semibold">
-            A dev environment for another branch appeared!
-          </div>
-          {flags.showDonut && <Donut {...flags} />}
-        </div>
-        <FlagValues values={flags} />
         {shouldInjectToolbar && <VercelToolbar />}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 
+import { FlagValues } from "@vercel/flags/react";
+import { Donut } from "./Donut";
+import { getFlags } from "./getFlags";
+
 export default async function page() {
+  const flags = await getFlags();
   return (
     <div className="h-screen w-full flex flex-col items-center justify-center">
       <div className="text-xl font-semibold">
-        This is my mainline branch deploy
+        A dev environment for another branch appeared!
       </div>
+      {flags.showDonut && <Donut {...flags} />}
+      <FlagValues values={flags} />
     </div>
   );
 }
